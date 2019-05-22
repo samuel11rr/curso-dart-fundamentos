ea9a4f436e93865c51cc6bd46fc1b8ff94902497
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-05-22 18:02:38 -0500

    EJERCICIOS DE DART

---
+++ 13-futures.dart
@@ -0,0 +1,25 @@
+// FUTURES (promesas)
+
+void main() {
+  
+	print('Estamos a punto de pedir datos');
+  
+  httpGet('https://api.nasa.com/aliens').then( (data) {
+    
+    print( data );
+    
+  });
+  
+  
+  print('Ultima línea');
+}
+
+
+
+Future<String> httpGet(String url) {
+  
+  return Future.delayed( new Duration( seconds: 4 ), () {
+    return 'Hola Mundo';
+  });
+  
+}
